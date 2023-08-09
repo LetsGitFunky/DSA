8f0cca7168d239a42873f30f351642fe01a5637d
LetsGitFunky <NickHeinBass@gmail.com>
2023-08-09 12:45:11 -0700

    intersection (set)

--- intersection.js
+++ intersection.js
@@ -1,12 +1,26 @@
+// const intersection = (a, b) => {
+//     let obj = {};
+//     let result = []
+//     let ab = a.concat(b);
+//     for (let i = 0; i < ab.length; i++) {
+//         if (obj[ab[i]]) {
+//             result.push(ab[i])
+//         }
+//         obj[ab[i]] = 1
+//     }
+//     return result
+// };
+
 const intersection = (a, b) => {
-    let obj = {};
-    let result = []
-    let ab = a.concat(b);
-    for (let i = 0; i < ab.length; i++) {
-        if (obj[ab[i]]) {
-            result.push(ab[i])
-        }
-        obj[ab[i]] = 1
+    let result = [];
+    let setA = new Set();
+    for (let numA of a) {
+        setA.add(numA)
     }
-    return result
-  };
+    for (let numB of b) {
+        if (setA.has(numB)) {
+            result.push(numB)
+        }
+    }
+    return result;
+};
