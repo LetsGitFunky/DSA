ac398efa291826e0f251d8ef0a757125a622bddd
LetsGitFunky <NickHeinBass@gmail.com>
2023-08-14 19:36:07 -0700

    linkedListValues

---
+++ linkedListValues.js
@@ -0,0 +1,30 @@
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+const linkedListValues = (head) => {
+    const values = [];
+    let current = head;
+    while (current !== null) {
+        values.push(current.val);
+        current = current.next;
+    }
+    return values;
+};
+
+
+// const a = new Node("a");
+// const b = new Node("b");
+// const c = new Node("c");
+// const d = new Node("d");
+
+// a.next = b;
+// b.next = c;
+// c.next = d;
+
+// // a -> b -> c -> d
+
+// linkedListValues(a); // -> [ 'a', 'b', 'c', 'd' ]
